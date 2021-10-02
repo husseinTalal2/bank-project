2968bae48e11a4ff7c152eb8f6af538d7e84c092
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-10-03 01:20:14 +0300

    adding person class and fixing package errors

--- src/Classes/Person.java
+++ src/Classes/Person.java
@@ -1,3 +1,60 @@
+package Classes;
+
+import java.util.Random;
+
 public class Person {
+    int id;
+	String name;
+	String email;
+	String address;
+	char gender;
+
+    public Person(String name, String email, String address, char gender) {
+        Random rand = new Random();
+        int maxNumber = 999999;
+
+        this.id = rand.nextInt(maxNumber) + 1;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.gender = gender;
+    }
+
+   
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+    
+
+
     
 }
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Classes.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person("aikay aikay","email","ishdc",'f');
+        System.out.println(p.getName());
+        System.out.println(p.getEmail());
+        System.out.println(p.getAddress());
+    }
+}

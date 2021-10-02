e8348c9975b72f89351e1142d7b70b57911419c0
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-10-03 01:31:29 +0300

    adding pretteir to the settings

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Classes.Person;
 
 public class Main {
     public static void main(String[] args) {
-        Person p = new Person("aikay aikay","email","ishdc",'f');
+        Person p = new Person("aikay aikay", "email", "ishdc", 'f');
         System.out.println(p.getName());
         System.out.println(p.getEmail());
         System.out.println(p.getAddress());

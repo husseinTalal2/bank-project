436932b249b02c1cd50fc3beb1fdf09d086992a4
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-10-04 12:10:43 +0300

    adding the main methods of User class

--- src/Classes/User.java
+++ src/Classes/User.java
@@ -1,4 +1,54 @@
 package Classes;
-public class User {
-    
+
+import java.util.ArrayList;
+
+public class User extends Person {
+    double balance = 0;
+    ArrayList<Integer> sendingTransactionIds = new ArrayList<>();
+    ArrayList<Integer> receivingTransactionIds = new ArrayList<>();
+
+    public User(String name, String email, String address, char gender) {
+        super(name, email, address, gender);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        if (amount <= 0) {
+            System.out.println("The amount of deposited money should be greater than zero!");
+        } else {
+            this.balance += amount;
+        }
+    }
+
+    public void withdraw(int amount) {
+        if ((balance - amount) < 0) {
+            System.out.println(
+                    "The amount of withdrawn money is greater than the current balance, try again with a decent amount");
+        } else {
+            this.balance -= amount;
+        }
+    }
+
+    public void addSendingTransactionId(int id) {
+        sendingTransactionIds.add(id);
+    }
+
+    public void addReceivingTransactionId(int id) {
+        receivingTransactionIds.add(id);
+    }
+
+    public void getSendingTransactions() {
+
+    }
+
+    public void getReceivingTransactions() {
+
+    }
+
+    public void getHistory() {
+
+    }
 }

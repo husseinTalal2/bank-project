2154d54198f34fda8c1a5a719e7f9e13cf0696fe
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-10-05 01:18:28 +0300

    experminting

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.util.HashMap;
+
 import Classes.Person;
+import Classes.Transaction;
 import Classes.User;
 
 public class Main {
@@ -8,9 +11,19 @@ public class Main {
         System.out.println(p.getEmail());
         System.out.println(p.getAddress());
         p.deposit(5000);
-        p.addSendingTransactionId(156);
-        p.addSendingTransactionId(789);
-        p.addSendingTransactionId(8754);
-        p.getSendingTransactions();
+
+        // p.getSendingTransactions();
+        HashMap<Integer, Transaction> transactions = new HashMap<Integer, Transaction>();
+
+        Transaction t = new Transaction(p.getId(), 156, 4000);
+        Transaction t2 = new Transaction(p.getId(), 456, 500);
+
+        System.out.println(t.getDate());
+        p.addSendingTransactionId(t.getId());
+        p.addSendingTransactionId(t2.getId());
+        transactions.put(t.getId(), t);
+        transactions.put(t2.getId(), t2);
+
+        System.out.println(p.getSendingTransactions(transactions));
     }
 }

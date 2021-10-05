cec9dc36ca7ca88882d4cce169b1faad1acff575
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-10-05 11:11:04 +0300

    playground in main

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,33 @@ import Classes.User;
 
 public class Main {
     public static void main(String[] args) {
+        HashMap<Integer, Transaction> transactions = new HashMap<Integer, Transaction>();
+        HashMap<Integer, User> users = new HashMap<Integer, User>();
         User p = new User("aikay aikay", "email", "ishdc", 'f');
-        System.out.println(p.getName());
-        System.out.println(p.getEmail());
-        System.out.println(p.getAddress());
+        users.put(p.getId(), p);
+        User p2 = new User("aikay aikay", "email", "ishdc", 'f');
+        users.put(p2.getId(), p2);
+
+        // System.out.println(p.getName());
+        // System.out.println(p.getEmail());
+        // System.out.println(p.getAddress());
         p.deposit(5000);
+        p2.deposit(5000);
 
         // p.getSendingTransactions();
-        HashMap<Integer, Transaction> transactions = new HashMap<Integer, Transaction>();
 
         Transaction t = new Transaction(p.getId(), 156, 4000);
-        Transaction t2 = new Transaction(p.getId(), 456, 500);
+        Transaction t2 = new Transaction(p2.getId(), p.getId(), 500);
 
-        System.out.println(t.getDate());
+        // System.out.println(t.getDate());
         p.addSendingTransactionId(t.getId());
-        p.addSendingTransactionId(t2.getId());
+        p.addReceivingTransactionId(t2.getId());
+        p2.addSendingTransactionId(t2.getId());
         transactions.put(t.getId(), t);
         transactions.put(t2.getId(), t2);
+        p.getHistory(transactions, users);
+        System.out.println();
+        p2.getHistory(transactions, users);
 
-        System.out.println(p.getSendingTransactions(transactions));
     }
 }

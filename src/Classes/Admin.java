cf1d55b6191a8e6c8987f70f7825d530a94ce545
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-10-05 11:29:20 +0300

    adding admin methods

--- src/Classes/Admin.java
+++ src/Classes/Admin.java
@@ -1,4 +1,22 @@
 package Classes;
+
+import java.util.HashMap;
+
 public class Admin {
-    
+    public void deposit(int userId, double amount, HashMap<Integer, User> users) {
+        User user = Utils.getUser(userId, users);
+        user.deposit(amount);
+    }
+
+    public void withdraw(int userId, double amount, HashMap<Integer, User> users) {
+        User user = Utils.getUser(userId, users);
+        user.withdraw(amount);
+    }
+
+    public void editUser(int userId, String changeType, String changeValue) {
+        switch (changeType) {
+            
+        }
+
+    }
 }

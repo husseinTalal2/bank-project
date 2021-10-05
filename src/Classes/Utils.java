aa76a61307573731364c26102f2fa605c8dea6fc
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-10-05 11:10:52 +0300

    adding functions to utils class

--- src/Classes/Utils.java
+++ src/Classes/Utils.java
@@ -1,4 +1,12 @@
 package Classes;
-public class Utils {
-    
+
+import java.util.HashMap;
+
+class Utils {
+    public static User getUser(int id, HashMap<Integer, User> users) {
+        return users.get(id);
+    }
+    public static String getUserName(int id, HashMap<Integer, User> users) {
+        return users.get(id).getName();
+    }
 }
